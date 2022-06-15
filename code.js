500b69af3c2edce2a4455e9d7f32e90db4be484d
ShaharYemini <76625369+ShaharYemini@users.noreply.github.com>
2022-06-15 23:56:57 +0300

    חיבור למשתמש גוגל

---
+++ code.js
@@ -0,0 +1,33 @@
+function signOut() {
+    google.accounts.id.disableAutoSelect();
+    console.log(1)
+    $(".data").css("display", "none");
+    $(".g_id_signin").css("display", "block");
+}
+function onSignIn(response) {
+     const responsePayload = decodeJwtResponse(response.credential);
+
+     /*console.log("ID: " + responsePayload.sub);
+     console.log('Full Name: ' + responsePayload.name);
+     console.log('Given Name: ' + responsePayload.given_name);
+     console.log('Family Name: ' + responsePayload.family_name);
+     console.log("Image URL: " + responsePayload.picture);
+     console.log("Email: " + responsePayload.email);*/
+
+     $("#name").text(responsePayload.name);
+    $("#email").text(responsePayload.email);
+    $("#user-icon").attr('src', responsePayload.picture);
+    $(".data").css("display", "flex");
+    $(".g_id_signin").css("display", "none");
+}
+
+function decodeJwtResponse (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+};
+
